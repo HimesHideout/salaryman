a1dc17a226093e4fc3a478070c4559844444d117
Oliver Crossley <ollythecrossley@outlook.com>
2023-10-19 14:20:19 +0100

    sets up bot run file

---
+++ index.js
@@ -0,0 +1,12 @@
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+require("dotenv").config()
+
+const token = process.env.DISCORD_TOKEN
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, c => {
+    console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(token);
