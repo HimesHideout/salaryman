853bc7545bff1fd16c19c0dd9100dd6372e0781b
Oliver Crossley <ollythecrossley@outlook.com>
2023-11-15 22:15:10 +0000

    allows reading of events from events folder

--- index.js
+++ index.js
@@ -7,12 +7,9 @@ const ENV = process.env.NODE_ENV
 const pathToCorrectFile = `${__dirname}/.env.${ENV}`;
 require("dotenv").config({ path: pathToCorrectFile });
 
-// require("dotenv").config()
-
 // Client Creation
 const token = process.env.DISCORD_TOKEN
 const client = new Client({ intents: [GatewayIntentBits["Guilds"]] });
-
 
 // Commands Handler
 client.commands = new Collection();
@@ -34,35 +31,18 @@ for (const folder of commandFolders) {
 }
 
 // On Ready
-client.once(Events.ClientReady, c => {
-    console.log(`Ready! Logged in as ${c.user.tag}`);
-});
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-
-// On Command
-client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-    // console.log(interaction);
-
-    const command = interaction.client.commands.get(interaction.commandName);
-
-    if (!command) {
-        console.error(`No command matching ${interaction.commandName} was found.`);
-        return;
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
     }
-
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        if (interaction.replied || interaction.deferred) {
-            await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-        } else {
-            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-        }
-    }
-});
-
+}
 
 // Ready Up Bot
 client.login(token);
